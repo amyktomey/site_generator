02e601a29276759d152ecacdb87395f75d7b1195
Amy Tomey <amy.tomey59@gmail.com>
2021-09-29 17:47:12 -0400

    initial commit

---
+++ ssg/site.py
@@ -0,0 +1,8 @@
+ from pathlib import Path
+
+class Site:
+    def __site__(self, source, dest):
+        self.source = Path()
+        self.dest = Path()
+    def create_dir(self, path):
+        directory = "C:\Users\m0mon\OneDrive\Desktop\Python2021\site_gen\ssg"
